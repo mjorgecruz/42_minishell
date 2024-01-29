d709ba431aa12a70b7452fd37162b6806e433a34
masoares <manueljorgecruz@hotmail.com>
2024-01-29 16:46:09 +0000

    pipe opens new line

--- include/history.c
+++ include/history.c
@@ -6,7 +6,7 @@
 /*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 16:12:32 by masoares          #+#    #+#             */
-/*   Updated: 2024/01/29 11:55:01 by masoares         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:40:49 by masoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 
 #include "minishell.h"
 
-char	*get_line(char *line_read)
+char	*get_line(char *total_line)
 {
-	if (line_read && *line_read)
-		line_read = (char *) NULL;
+	char	*line_read;
+	char	*vai_fora;
+	
+	vai_fora = NULL;
 	line_read = readline("minishell > ");
-	if (line_read && *line_read)
-		add_history(line_read);
+	total_line = line_read;
+	while (line_read[(int) ft_strlen(line_read) -1] == '|')
+	{
+		line_read = readline("> ");
+		vai_fora = total_line;
+		total_line = ft_strjoin(total_line, line_read);
+		free(vai_fora);
+	}
+	total_line = ft_strjoin(total_line, line_read);
+	if (total_line && *total_line)
+		add_history(total_line);
 	return (line_read);
 }
+
