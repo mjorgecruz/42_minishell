bbb306a597fd2ce77834d5c4b53ee82b2bfda245
Filipe <luis-ffe@student.42porto.com>
2024-02-22 23:10:25 +0000

    another

--- include/expansive.c
+++ include/expansive.c
@@ -1,8 +1,148 @@
 #include "minishell.h"
 
-/*VER LINHA 140*/
+char	*get_ds_code(char *cmd)
+{
+	char	*tmp;
+	int		len;
+
+	len = 0;
+	tmp = ft_strnstr(cmd, "$", ft_strlen(cmd)); //encontra $ returna tudo na sua frente
+	if (tmp[1] == '?')
+		return (ft_strdup("$?")); //se for interrogacao retorna isso
+	if (ft_isdigit(tmp[1]))
+		return (ft_substr(tmp, 0, 1)); //se for digito not valid RETURN NULL
+	while (ft_isalnum(tmp[len + 1]) || tmp[len + 1] == '_') //enquanto for alphanumerico ou _ is valid!
+		len++;
+	return (ft_substr(tmp, 1, len));
+}
+
+char	*ds_replace_codeword(char *cmd, char *code, char *env_val)
+{
+	char	*new;
+	char	*start;
+	char	*end;
+	int		len;
+
+	if (!env_val)
+		env_val = "";
+	len = ft_strlen(cmd) - ft_strlen(code) + ft_strlen(env_val) + 1;
+	new = (char *)malloc(sizeof(char) * len);
+	if (!new)
+		return (NULL);
+	start = ft_strnstr(cmd, "$", ft_strlen(cmd)) + 1;
+	end = start + ft_strlen(code);
+	if (*start == '?')
+		end--;
+	ft_strlcpy(new, cmd, start - cmd);
+	ft_strlcat(new, env_val, len);
+	ft_strlcat(new, end, len);
+    free(code);
+    free(cmd);
+	return (new);
+}
+
+bool is_str_empty(char *str)
+{
+	if (!str)
+		return (true);
+	if (*str == '\0' || *str == '\n' || *str == '\t')
+		return (true);
+	return (false);
+}
+
+int	quotes_counter(char *cmd)
+{
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = cmd[i];
+	while (cmd[i] && cmd[i] == quote)
+		i++;
+	return (i);
+}
+
+/*
+
+necessita receber o comando dividido
+'$HOME'"$HOME"$HOME
+tenho de separa o que esta dentro de singles do restante e depois tenho de remover
+as aspas
+
+*/
+
+char	*expand_single_variable(char *cmd)
+{
+	char	*env;
+	char	*code_word;
+	char	*expanded_str;
+
+	expanded_str = ft_strdup(cmd);
+	if (*expanded_str == '\'' && quotes_counter(expanded_str) % 2)               //no caso de haver single quotes em numero impar retorna como esta.
+		return (expanded_str);
+	while (ft_strnstr(expanded_str, "$", ft_strlen(expanded_str)))
+	{
+		code_word = get_ds_code(expanded_str);
+		if (!code_word || is_str_empty(code_word))
+			break ;                                                             //sendo nula ou vazia
+		if (!ft_strncmp(code_word, "$?", 2))
+			env = "696969";                                                     // codigo de erro???? pid? o que ???
+		else
+			env = getenv(code_word);                                            // teremos de substituir isto pela nossa propria versao!  ft_getenv!!!!
+		expanded_str = ds_replace_codeword(expanded_str, code_word, env);
+	}
+	return (expanded_str);
+}
 
 
+typedef struct s_lstexpand {
+    int id;
+    int status;
+    char *content;
+    struct s_lstexpand *next;
+} t_lstexpand;
+
+t_lstexpand *create_node_lstexpand(int id, int status, char *content)
+{
+    t_lstexpand *new_node;
+    
+    new_node = (t_lstexpand *)malloc(sizeof(t_lstexpand));
+    if (!new_node)
+        return (NULL);
+    new_node->id = id;
+    new_node->status = status;
+    new_node->content = content;
+    new_node->next = NULL;
+    return new_node;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+    char input_cmd[100];
+    
+    printf("EXPANDER_Minishell/> ");
+    fgets(input_cmd, sizeof(input_cmd), stdin);
+    input_cmd[strcspn(input_cmd, "\n")] = '\0'; 
+    char *result = expand_single_variable(input_cmd);
+    printf("Expanded command: %s\n", result);
+    free(result);
+    return 0;
+}
 
 // typedef struct s_expstr
 // {
@@ -138,140 +278,8 @@ cc expansive.c -o expansive -Ilibft -Llibft -lf
 write comands with $ at the start
 
 make sure the smae functions are comented in the main minishell
-
 */
 
-
-//////////////////////////////////////////////
-
-
-
-/* returns string with the code word in the dollarsign */
-/*ALREADY VERIFIES VALIDITY OF THE CHARS FOLLOWING THE $ */
-/*DOES not handle the aspens just grabs what is after the $ */
-char	*get_ds_code(char *cmd)
-{
-	char	*tmp;
-	int		len;
-
-	len = 0;
-	tmp = ft_strnstr(cmd, "$", ft_strlen(cmd)); //encontra $ returna tudo na sua frente
-	if (tmp[1] == '?')
-		return (ft_strdup("$?")); //se for interrogacao retorna isso
-	if (ft_isdigit(tmp[1]))
-		return (ft_substr(tmp, 0, 1)); //se for digito not valid RETURN NULL
-	while (ft_isalnum(tmp[len + 1]) || tmp[len + 1] == '_') //enquanto for alphanumerico ou _ is valid!
-		len++;
-	return (ft_substr(tmp, 1, len));
-}
-
-/*substitui na string cmd   a codeword pela respetiva env_val
-
-se nao for encontrada a env val da problems 
-ver como fazer o management disso nas funcoes superiores que enviam os argumentos para esta
-*/
-
-char	*ds_replace_codeword(char *cmd, char *code, char *env_val)
-{
-	char	*new;
-	char	*start;
-	char	*end;
-	int		len;
-
-	if (!env_val)
-		env_val = "";
-	len = ft_strlen(cmd) - ft_strlen(code) + ft_strlen(env_val) + 1;
-	new = (char *)malloc(sizeof(char) * len);
-	if (!new)
-		return (NULL);
-	start = ft_strnstr(cmd, "$", ft_strlen(cmd)) + 1;
-	end = start + ft_strlen(code);
-	if (*start == '?')
-		end--;
-	ft_strlcpy(new, cmd, start - cmd);
-	ft_strlcat(new, env_val, len);
-	ft_strlcat(new, end, len);
-	return (new);
-}
-
-//no whitespaces as first char!!!
-bool is_str_empty(char *str)
-{
-	if (!str)
-		return (true);
-	if (*str == '\0' || *str == '\n' || *str == '\t')
-		return (true);
-	return (false);
-}
-
-//tambem podia ser boolean
-int	quotes_counter(char *cmd)
-{
-	int		i;
-	char	quote;
-
-	i = 0;
-	quote = cmd[i];
-	while (cmd[i] && cmd[i] == quote)
-		i++;
-	return (i);
-}
-
-
-char	*expand_single_variable(char *cmd)
-{
-	char	*env;
-	char	*code_word;
-	char	*expanded_str;
-
-	expanded_str = ft_strdup(cmd);
-	if (*expanded_str == '\'' && quotes_counter(expanded_str) % 2)               //no caso de haver single quotes em numero impar retorna como esta.
-		return (expanded_str);
-	while (ft_strnstr(expanded_str, "$", ft_strlen(expanded_str)))
-	{
-		code_word = get_ds_code(expanded_str);
-		if (!code_word || is_str_empty(code_word))
-			break ;                                                             //sendo nula ou vazia
-		if (!ft_strncmp(code_word, "$?", 2))
-			env = "696969";                                                     // codigo de erro???? pid? o que ???
-		else
-			env = getenv(code_word);                                            // teremos de substituir isto pela nossa propria versao!  ft_getenv!!!!
-		expanded_str = ds_replace_codeword(expanded_str, code_word, env);
-		free(code_word);
-		if (env)
-			free(env);
-	}
-	return (expanded_str);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main(void)
-{
-    char input_cmd[100];
-    
-    printf("EXPANDER_Minishell/> ");
-    fgets(input_cmd, sizeof(input_cmd), stdin);
-    input_cmd[strcspn(input_cmd, "\n")] = '\0'; 
-    char *result = get_ds_code(input_cmd);
-    printf("Expanded command: %s\n", result);
-    return 0;
-}
 
 // int main(void)
 // {
