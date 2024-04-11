b89cd2ac21b13acfd1ad2cd42d417558395956af
masoares <manueljorgecruz@hotmail.com>
2024-04-11 10:28:16 +0100

    heredocs with correct number of lines per function

--- srcs/heredocs/heredocs.c
+++ srcs/heredocs/heredocs.c
@@ -6,11 +6,15 @@
 /*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 18:03:31 by masoares          #+#    #+#             */
-/*   Updated: 2024/04/08 12:59:42 by masoares         ###   ########.fr       */
+/*   Updated: 2024/04/11 10:27:48 by masoares         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../../includes/minishell.h"
+
+static int	heredoc_reader(char ***new_heredocs, int *cur_heredocs, int fd);
+static int	*forking_heredocs(char *final_str, char *line_read, int n_letters, int i);
+static int write_to_fd(char *final_str, char *line_read,int *fd, int n_letters);
 
 int	heredoc_counter(char *line_read, int i)
 {
@@ -91,97 +95,86 @@ void	add_heredocs(char ***new_heredocs, int cur_heredocs, char *line_read, int i
 }
 int heredoc_creator(char ***new_heredocs, int *cur_heredocs, char *line_read, int i)
 {
-	char	*str;
 	char	*final_str;
 	int		n_letters;
-	int		j;
-	int		fd[2];
-	int		pid;
-	int		bread;
-	char	*buffer;
-	char	*temp;
+	int		*fd;
 
 	final_str = NULL;
-	bread = 0;
 	i = ignore_spaces(line_read, i + 1);
 	n_letters = 0;
-	while (line_read[i + n_letters] != ' ' && !is_special_char(line_read[i+ n_letters])
+	(*new_heredocs)[*cur_heredocs] = NULL;
+	if (ft_strchr("\'\"", line_read[i]))
+		i++;
+	fd = forking_heredocs(final_str, line_read, n_letters, i);
+	heredoc_reader(new_heredocs, cur_heredocs, fd[0]);
+	free(fd);
+	return (i);
+}
+
+static int	*forking_heredocs(char *final_str, char *line_read, int n_letters, int i)
+{
+	int		pid;
+	int		*fd;
+	
+	fd = ft_calloc(2, sizeof(int));
+	// j = 0;
+	n_letters = 0;
+	if (ft_strchr("\'\"", line_read[i]))
+		i++;
+	while (!is_space(line_read[i + n_letters]) && !is_special_char(line_read[i+ n_letters])
 		&& line_read[i + n_letters] != '\0' && !ft_strchr("\'\"", line_read[i + n_letters]))
 		n_letters++;
-	(*new_heredocs)[*cur_heredocs] = NULL;
 	pipe(fd);
 	pid = fork();
 	if (pid == 0)
 	{
 		switch_sig_default();
-		close(fd[0]);
-		str = ft_calloc(n_letters + 1, sizeof(char));
-		j = 0;
-		while (j < n_letters)
-			str[j++] = line_read[i++];
-		add_partials(&(final_str), str);
-		write(fd[1], final_str, ft_strlen(final_str));
-		free(str);
-		free(final_str);
+		write_to_fd(final_str, &line_read[i], fd, n_letters);
 		exit(EXIT_SUCCESS);
 	}
 	switch_sig_function();
 	waitpid(0, NULL, 0);
 	close(fd[1]);
+	return (fd);
+}
+
+static int write_to_fd(char *final_str, char *line_read,int *fd, int n_letters)
+{
+	char	*str;
+	int		j;
+	
+	j = 0;
+	close(fd[0]);
+	str = ft_calloc(n_letters + 1, sizeof(char));
+	while (j < n_letters)
+	{
+		str[j] = line_read[j];
+		j++;
+	}	
+	add_partials(&(final_str), str);
+	write(fd[1], final_str, ft_strlen(final_str));
+	free(str);
+	free(final_str);
+	return (0);
+}
+
+static int	heredoc_reader(char ***new_heredocs, int *cur_heredocs, int fd)
+{
+	int		bread;
+	char	*buffer;
+	char	*temp;
+		
 	buffer = ft_calloc(21, sizeof(char));
-	bread = read(fd[0], buffer, 20);
+	bread = read(fd, buffer, 20);
 	temp = ft_strdup("");
 	while (bread > 0)
 	{
 		temp = ft_strjoin_2(temp, buffer);
-		bread = read(fd[0], buffer, 20);
+		bread = read(fd, buffer, 20);
 		buffer[bread] = 0;
 	}
 	(*new_heredocs)[*cur_heredocs] = temp;
 	(*cur_heredocs)++;
 	free(buffer);
-	return (i);
-}
-
-void	add_newline_line(char **total_line, char *line_read)
-{
-	char *garbage;
-	
-	garbage = *total_line;
-	if (*total_line != NULL)
-	{
-		*total_line = ft_strjoin(*total_line, "\n");
-		free(garbage);
-		garbage = *total_line;
-		*total_line = ft_strjoin(*total_line, line_read);
-		free(garbage);
-	}
-	else
-		*total_line = line_read;
-}
-char	*ft_strjoin_2(char *s1, char const *s2)
-{
-	size_t	i1;
-	size_t	i2;
-	size_t	j;
-	char	*s;
-
-	j = 0;
-	i1 = ft_strlen(s1);
-	i2 = ft_strlen(s2);
-	s = (char *) malloc (sizeof(char) * (i1 + i2 + 1));
-	if (s == NULL)
-		return (NULL);
-	while (j < i1)
-	{
-		s[j] = s1[j];
-		j++;
-	}
-	while (j < i1 + i2)
-	{
-		s[j] = s2[j - i1];
-		j++;
-	}
-	s[j] = '\0';
-	return (free(s1), s);
+	return (0);
 }
--- srcs/heredocs/heredocs_2.c
+++ srcs/heredocs/heredocs_2.c
@@ -6,7 +6,7 @@
 /*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 23:25:24 by masoares          #+#    #+#             */
-/*   Updated: 2024/04/09 13:57:28 by masoares         ###   ########.fr       */
+/*   Updated: 2024/04/11 09:43:35 by masoares         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -36,4 +36,48 @@ void	add_partials(char **heredoc, char *str)
 	}	
 	else
 		*heredoc = ft_strjoin(*heredoc, "\n");
+}
+
+
+void	add_newline_line(char **total_line, char *line_read)
+{
+	char *garbage;
+	
+	garbage = *total_line;
+	if (*total_line != NULL)
+	{
+		*total_line = ft_strjoin(*total_line, "\n");
+		free(garbage);
+		garbage = *total_line;
+		*total_line = ft_strjoin(*total_line, line_read);
+		free(garbage);
+	}
+	else
+		*total_line = line_read;
+}
+char	*ft_strjoin_2(char *s1, char const *s2)
+{
+	size_t	i1;
+	size_t	i2;
+	size_t	j;
+	char	*s;
+
+	j = 0;
+	i1 = ft_strlen(s1);
+	i2 = ft_strlen(s2);
+	s = (char *) malloc (sizeof(char) * (i1 + i2 + 1));
+	if (s == NULL)
+		return (NULL);
+	while (j < i1)
+	{
+		s[j] = s1[j];
+		j++;
+	}
+	while (j < i1 + i2)
+	{
+		s[j] = s2[j - i1];
+		j++;
+	}
+	s[j] = '\0';
+	return (free(s1), s);
 }
