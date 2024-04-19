090d2f79ab96d23c41e3ec62c7ae642300e446eb
Luis Filipe Fernandes Carvalho <luis-ffe@c2r2s3.42porto.com>
2024-04-19 14:36:23 +0100

    saSS

--- includes/signals.h
+++ includes/signals.h
@@ -1,24 +1,22 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luis-ffe <luis-ffe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 17:02:47 by masoares          #+#    #+#             */
-/*   Updated: 2024/04/17 11:31:32 by masoares         ###   ########.fr       */
+/*   Updated: 2024/04/19 14:25:59 by luis-ffe         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
-
 
 void	switch_sig_function(void);
 void	switch_sig_new(void);
 void	switch_sig_default(void);
 void	handle_sigint_status(void);
-
 void	update_status_sigint(int sig);
 void	update_status_sigquit(int sig);
 void	sig_handler_quit(int sig);
