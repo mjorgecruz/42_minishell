267ae5bb9e87acc909d5e50e488e792c77777e6f
masoares <manueljorgecruz@hotmail.com>
2024-04-19 10:53:51 +0100

    another one

--- srcs/execution/solver_2.c
+++ srcs/execution/solver_2.c
@@ -6,7 +6,7 @@
 /*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 16:49:36 by masoares          #+#    #+#             */
-/*   Updated: 2024/04/18 17:16:09 by masoares         ###   ########.fr       */
+/*   Updated: 2024/04/19 10:53:34 by masoares         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,12 +14,13 @@
 
 static int     define_output_append(char *cmds, int *i, int *fd, int *out);
 static int     define_output_redir(char *cmds, int *i, int *fd, int *out);
+static char	*is_file(int *i, int *fd, int *in, t_command *cmds);
+static void	is_heredoc(int *fd, int *heredocs, int *in, int *i);
 
 int	define_input(t_command *cmds, int *fd, int *heredocs, int *in)
 {
 	int		i;
 	char	*file;
-	int 	pos;
 		
 	i = 0;
 	file = NULL;
@@ -29,32 +30,49 @@ int	define_input(t_command *cmds, int *fd, int *heredocs, int *in)
 		i = ignore_in_quotes(cmds->cmds, i);
 		if ((cmds->cmds)[i] == '<' && (cmds->cmds)[i + 1] != '<')
 		{
-			i++;
-			pos = i;
 			if (file != NULL)
 				free(file);
-			file = create_file_name(cmds->cmds, &i);
-			*fd = open(file, O_RDONLY);
-			if (*fd < 0)
-				return(free(file), pos);
-			*in = IN_DOC;
+			file = is_file(&i, fd, in, cmds);
+			if (file == NULL)
+				return(i);
 		}
 		else if (cmds->cmds[i] == '<' && cmds->cmds[i + 1] == '<')
-		{
-			if (*fd > STDIN_FILENO)
-			{
-				close(*fd);
-				*fd = -1;
-			}
-			(*heredocs)++;
-			*in = HEREDOC;
-			i++;
-		}
+			is_heredoc(fd, heredocs, in, &i);
 		i++;
 	}
 	if (file != NULL)
 		free(file);
 	return (i);
+}
+
+static char	*is_file(int *i, int *fd, int *in, t_command *cmds)
+{
+	char	*file;
+	int		pos;
+	
+	(*i)++;
+	pos = (*i);
+	file = create_file_name(cmds->cmds, i);
+	*in = IN_DOC;
+	*fd = open(file, O_RDONLY);
+	if (*fd < 0)
+	{
+		*i = pos;
+		return(free(file), NULL);
+	}
+	return (file);
+}
+
+static void	is_heredoc(int *fd, int *heredocs, int *in, int *i)
+{
+	if (*fd > STDIN_FILENO)
+	{
+		close(*fd);
+		*fd = -1;
+	}
+	(*heredocs)++;
+	*in = HEREDOC;
+	(*i)++;
 }
 
 int	define_output(t_command *cmds, int *fd, int *out, int pos0)
