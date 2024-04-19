8d8587848a164e1a13e04e3472ff70f73ef8c74a
masoares <manueljorgecruz@hotmail.com>
2024-04-19 16:00:33 +0100

    pushens of the night

--- srcs/execution/out_setup_general2.c
+++ srcs/execution/out_setup_general2.c
@@ -6,27 +6,28 @@
 /*   By: masoares <masoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:46:52 by masoares          #+#    #+#             */
-/*   Updated: 2024/04/19 15:13:23 by masoares         ###   ########.fr       */
+/*   Updated: 2024/04/19 16:00:08 by masoares         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../../includes/minishell.h"
 
 static int		code_verifier(int res);
-static void	mega_piper(t_token *cmd_list, t_info info,
-		t_localenv *local, int *pid);
+static void	mega_piper(t_token *cmd_list, t_info info, int *pid);
+static void	pipe_exec(t_token *cmd_list, t_info info, int i, int *pid);
 
 int		mult_cmd_executer(t_token *cmd_list, t_info info,
 		t_localenv *local, int i)
 {
 	int			*pid;
 	int			res;
+	(void) local;
 
 	while (cmd_list->cmds[i].cmds)
 	 	i++;
 	pid = (int *)malloc(sizeof(int) * i);
 	i = 0;	
-	mega_piper(cmd_list, info, local, pid);
+	mega_piper(cmd_list, info, pid);
 	handle_sigint_status();
 	i = 0;
 	while (cmd_list->cmds[i].cmds)
@@ -38,15 +39,12 @@ int		mult_cmd_executer(t_token *cmd_list, t_info info,
 	return(free(pid), res);
 }
 
-static void		mega_piper(t_token *cmd_list, t_info info,
-		t_localenv *local, int *pid)
+static void		mega_piper(t_token *cmd_list, t_info info, int *pid)
 {
 	int		fd[2];
-	int		res;
 	int		i;
 	int		stdin;
 	
-	res = 0;
 	i = 0;
 	stdin = dup(STDIN_FILENO);
 	while (cmd_list->cmds[i].cmds)
@@ -59,11 +57,7 @@ static void		mega_piper(t_token *cmd_list, t_info info,
 			{
 				switch_sig_default();
 				pied_piper(cmd_list, fd, i, &stdin);
-				free(pid);
-				res = inter_executioner(cmd_list, info, local, i);
-				tree_cleaner(info.head);
-				free_info_andenv(info);
-				exit(res);
+				pipe_exec(cmd_list, info, i, pid);
 			}
 			dup2(fd[0], stdin);
 			close(fd[0]);
@@ -74,6 +68,16 @@ static void		mega_piper(t_token *cmd_list, t_info info,
 	close(stdin);
 }
 
+static void	pipe_exec(t_token *cmd_list, t_info info, int i, int *pid)
+{
+	int	res;
+	
+	free(pid);
+	res = inter_executioner(cmd_list, info, info.local, i);
+	tree_cleaner(info.head);
+	free_info_andenv(info);
+	exit(res);
+}
 
 int		pied_piper(t_token *cmd_list, int *fd, int i, int *stdin)
 {
